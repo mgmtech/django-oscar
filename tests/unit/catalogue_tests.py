99795fce5b0a715a78e04458763f3646f4f913a3
Matt Miller <matt@flashnotes.com>
2013-03-11 17:34:45 -0400

    Backport of pickle fix

--- tests/unit/catalogue_tests.py
+++ tests/unit/catalogue_tests.py
@@ -220,3 +220,27 @@ class ProductAttributeCreationTests(TestCase):
         invalid_option.option = 'invalid option'
         self.assertRaises(ValidationError, pa.get_validator(),
                           invalid_option)
+
+class TestProductPickling(TestCase):
+    def setUp(self):
+        self.product_class, _ = ProductClass.objects.get_or_create(
+            name='Pickle')
+
+    def test_product_pickling_works(self):
+        import pickle
+        from StringIO import StringIO
+
+        src = StringIO()
+        pickler = pickle.Pickler(src)
+
+        product = Product.objects.create(title='test_pickle_product',
+                                         product_class=self.product_class)
+
+        pickler.dump(product)
+
+        dst = StringIO(src.getvalue())
+        unpickler = pickle.Unpickler(dst)
+
+        product_pickle = unpickler.load()
+
+        pickler.dump(product_pickle)
